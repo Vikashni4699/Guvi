2e552523b174970993befaf92b8a552d2d49ec70
Vikashni4699 <43268583+Vikashni4699@users.noreply.github.com>
2019-08-17 12:18:45 +0530

    Update set1-10.py

--- set1-10.py
+++ set1-10.py
@@ -1,7 +1,7 @@
-Number = int(input())
+Number1= int(input())
 Count = 0
-while(Number > 0):
-    Number = Number // 10
+while(Number1 > 0):
+    Number1 = Number1 // 10
     Count = Count + 1
 
 print(Count)
